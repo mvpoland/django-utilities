1f9b5bd3e5cceccbe406de4c4560c51e9a9fc922
Maarten Timmerman <maarten.timmerman@city-live.be>
2011-02-25 14:58:23 +0100

    setup the project

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-utilities",
+    version = "0.1",
+    url = 'https://github.com/citylive/django-utilities',
+    license = 'BSD',
+    description = "Lots of useful small utilities",
+    long_description = open('README','r').read(),
+    author = 'City Live NV',
+    packages = find_packages('src'),
+    package_data = {'utilities': [
+					'templates/*.html', 'templates/*/*.html', 'templates/*/*/*.html'
+                ],},
+    zip_safe=False, # Don't create egg files, Django cannot find templates in egg files.
+    include_package_data=True,
+    package_dir = {'': 'src'},
+    classifiers = [
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)
+
