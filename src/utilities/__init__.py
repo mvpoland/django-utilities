77e0a8b7952fae32db2cd9e52f18cfb2e70c3933
Maarten Timmerman <maarten.timmerman@city-live.be>
2011-03-03 16:32:40 +0100

    added is_years_ago

--- src/utilities/__init__.py
+++ src/utilities/__init__.py
@@ -1,4 +1,3 @@
-
 VERSION = (0, 1,)
 
 # Dynamically calculate the version based on VERSION tuple
@@ -8,3 +7,21 @@ else:
     str_version = "%s.%s" % VERSION[:2]
 
 __version__ = str_version
+
+import datetime
+
+def is_years_ago(verify_date, years, start_date=None):
+    if start_date is None:
+        if isinstance(verify_date, datetime.datetime):
+            start_date = datetime.datetime.now()
+        else:
+            start_date = datetime.date.today()
+
+    try:
+        start_date = start_date.replace(year=start_date.year - years)
+    except ValueError:
+        if start_date.month == 2 and start_date.day == 29:
+            start_date = start_date.replace(month=2, day=28,
+                                            year=start_date.year - years)
+
+    return (verify_date - start_date).days > 0
