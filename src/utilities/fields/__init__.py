ad39651a2e9158cad0f84905d4c62d0d37f8d89a
wbrebels <werner.brebels@mobilevikings.com>
2012-05-14 12:27:07 +0200

    Added Enterprise number field

--- src/utilities/fields/__init__.py
+++ src/utilities/fields/__init__.py
@@ -1,2 +1,2 @@
 from utilities.fields.json_fields import JSONField
-from utilities.fields.form_fields import VATField, IBANField
+from utilities.fields.form_fields import EnterpriseNumberField, VATField, IBANField
